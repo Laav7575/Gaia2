92a18696f4cf6be2d2d14e0eb8847b0381905756
Laavanya Thiagalingam <109570028+Laav7575@users.noreply.github.com>
2024-01-14 04:51:42 -0500

    Add files via upload

---
+++ LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Gaia
+//
+//  Created by Shubhaangan Manoranjan on 2022-07-30.
+//
+
+import UIKit
+import AVKit
+import Vision
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+    
+   
+}
+
